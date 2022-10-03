d411ea848090009f9a4bb04408941f11f54d1be8
Uliana Ryzhkova <ulanaryzkova23417@gmail.com>
2022-10-03 16:10:49 +0300

    update 2

--- epli/app/src/main/java/com/example/epli/MainActivity.kt
+++ epli/app/src/main/java/com/example/epli/MainActivity.kt
@@ -3,29 +3,29 @@ package com.example.epli
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.epli.ui.theme.AppTheme
+import androidx.compose.runtime.SideEffect
+import com.example.epli.ui.screens.ApplicationScreen
 import com.example.epli.ui.theme.EpliTheme
+import com.example.epli.ui.theme.Palette
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EpliTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Text(text = "Hello lgbtq", color = AppTheme.colors.blue)
+                val systemUiController = rememberSystemUiController()
+                SideEffect {
+                    systemUiController.setSystemBarsColor(
+                        color = Palette.black,
+                        darkIcons = false
+                    )
+                }
+                ApplicationScreen()
                 }
             }
         }
     }
-}
+
